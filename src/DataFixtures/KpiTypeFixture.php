6570cc1ba8a3a197f0cde7e279f3f457b2b90b12
ronykader <rony.ksr06@gmail.com>
2023-11-27 15:58:37 +0600

    km-41: Department, Period, Kpi Type, Kpi Setup, Individual Kpi Fixtures added

---
+++ src/DataFixtures/KpiTypeFixture.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\KpiType;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class KpiTypeFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $date = new \DateTime('now', new \DateTimeZone('Asia/Dhaka'));
+        for ($i = 1; $i < 6; ++$i) {
+            $kpiType = new KpiType();
+            $kpiType->setTitle('Kpi Type' . $i);
+            $kpiType->setSlug('kpi-type-' . $i);
+            $kpiType->setStatus(1);
+            $kpiType->setCreatedAt($date);
+            $kpiType->setUpdatedAt($date);
+            $manager->persist($kpiType);
+        }
+        $manager->flush();
+    }
+}
